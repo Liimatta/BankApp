510aab27c05bf5dbea56ff970f2a00d60f016b7f
Janne Liimatta <janneliimatta@hotmail.com>
2018-03-28 19:37:05 +0300

    ibantzk

--- IBANCheck/IBANCheck/wtf.cs
+++ IBANCheck/IBANCheck/wtf.cs
@@ -11,23 +11,27 @@ namespace IBANCheck
             string bban = "123456785";
             string iban = bban + "1518";
             Console.WriteLine(iban);
-            int a = 0;
-            int b = 0;
             long Iban = Convert.ToInt64(iban);
+            string IbanCopy = iban;
+            int x = 0;
+            long numVal = 0;
             do
             {
-                do
-                {
-                    string A = a.ToString();
-                    string B = b.ToString();
 
-                    iban = iban + A + B;
-                    b++;
+                
+                if(x<10)
+                {
+                    IbanCopy += '0' + x.ToString();
                 }
-                while (Iban % 97 != 1);
-                a++;
+                else
+                {
+                    IbanCopy += x.ToString();
+                }
+                x++;
+                numVal = Int64.Parse(IbanCopy);
             }
-            while (Iban % 97 != 1);
+            while (numVal % 97 != 1);
+
             iban = Iban.ToString();
             Console.WriteLine(iban);
         }
